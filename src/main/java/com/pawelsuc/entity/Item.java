5c68e6d6d67e283e870d54658e2127cb7038e523
Paweł Suchań <pawelsuchan94@gmail.com>
2023-08-03 18:45:08 +0200

    Preparing project

---
+++ src/main/java/com/pawelsuc/entity/Item.java
@@ -0,0 +1,40 @@
+package com.pawelsuc.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "item")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long idItem;
+
+    private String name;
+
+    private String price;
+
+    public Long getIdItem() {
+        return idItem;
+    }
+
+    public void setIdItem(Long idItem) {
+        this.idItem = idItem;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
