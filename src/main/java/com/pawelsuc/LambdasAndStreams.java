3b83b60fe295e978b548919642a08fe84c30307c
Paweł Suchań <pawelsuchan94@gmail.com>
2023-08-09 08:55:40 +0200

    Reduce and collect

--- src/main/java/com/pawelsuc/LambdasAndStreams.java
+++ src/main/java/com/pawelsuc/LambdasAndStreams.java
@@ -4,6 +4,7 @@ import org.apache.catalina.LifecycleState;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LambdasAndStreams {
 //    funkcja
@@ -32,18 +33,23 @@ public class LambdasAndStreams {
         List<Integer> numbers = Arrays.asList(52, 17, 44, 98, 76, 12, 69, 2137, 8761);
 //        wybrać elementy parzyste, pomnożyć je przez 2, zsumować i wyświetlić wynik
 
-        int result = 0;
-        for (int i : numbers) {
-            if (i % 2 == 0) {
-                result += i * 2;
-            }
-        }
-//        System.out.println(result);
+//        int result = 0;
+//        for (int i : numbers) {
+//            if (i % 2 == 0) {
+//                result += i * 2;
+//            }
+//        }
+////        System.out.println(result);
 
-        System.out.println(numbers.stream()
-                .filter(i -> i % 2 == 0)
-                .mapToInt(i -> i * 2)
-                .sum()
-                );
+//        System.out.println(numbers.stream()
+//                .filter(i -> i % 2 == 0)
+//                .mapToInt(i -> i * 2)
+//                .sum()
+//                );
+//        System.out.println(numbers.stream().reduce(0,(subtotal, element) -> subtotal + element));
+        List<Integer> collect = numbers.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+        System.out.println(collect);
+
+
     }
 }
