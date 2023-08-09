13682b289cde7b2d76c4fd0f95c57e14a974e70c
Paweł Suchań <pawelsuchan94@gmail.com>
2023-08-09 08:37:54 +0200

    Lambdas and streams

---
+++ src/main/java/com/pawelsuc/LambdasAndStreams.java
@@ -0,0 +1,49 @@
+package com.pawelsuc;
+
+import org.apache.catalina.LifecycleState;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LambdasAndStreams {
+//    funkcja
+//    nazwa
+    //    lista parametrów
+    //    ciało funkcji
+//    typ zwracany
+//    (lista parametów) -> <ciało funkcji>
+
+
+    public static void main(String[] args) {
+
+//        Thread thread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("Informacja z osobnego wątku");
+//            }
+//        });
+//        thread.start();
+//        System.out.println("Informacja z metody main");
+
+//        Thread thread = new Thread(() -> System.out.println("Informacja z osobnego wątku"));
+//        thread.start();
+//        System.out.println("Informacja z metody main");
+
+        List<Integer> numbers = Arrays.asList(52, 17, 44, 98, 76, 12, 69, 2137, 8761);
+//        wybrać elementy parzyste, pomnożyć je przez 2, zsumować i wyświetlić wynik
+
+        int result = 0;
+        for (int i : numbers) {
+            if (i % 2 == 0) {
+                result += i * 2;
+            }
+        }
+//        System.out.println(result);
+
+        System.out.println(numbers.stream()
+                .filter(i -> i % 2 == 0)
+                .mapToInt(i -> i * 2)
+                .sum()
+                );
+    }
+}
