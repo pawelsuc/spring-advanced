e51eac168bdba67beeac64982a44c2684f6097a7
Paweł Suchań <pawelsuchan94@gmail.com>
2023-08-11 07:06:23 +0200

    Custom implementation of inMemoryAuthentication part 3

--- src/main/java/com/pawelsuc/SecurityConfig.java
+++ src/main/java/com/pawelsuc/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.pawelsuc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,9 +10,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity(debug = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    AuthenticationProvider authenticationProvider;
+
+    @Autowired
+    public SecurityConfig(AuthenticationProvider authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
+
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("{noop}user").roles("USER");
+        auth.authenticationProvider(authenticationProvider);
     }
 }
--- src/main/java/com/pawelsuc/component/InMemoryAuthenticationProvider.java
+++ src/main/java/com/pawelsuc/component/InMemoryAuthenticationProvider.java
@@ -2,17 +2,21 @@ package com.pawelsuc.component;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
+@Component
 public class InMemoryAuthenticationProvider implements AuthenticationProvider {
 
     private static final String USERNAME_CANNOT_BE_NULL = "Username cannot be null";
     private static final String CREDENTIALS_CANNOT_BE_NULL = "Credentials cannot be null";
+    private static final String INCORRECT_PASSWORD = "Incorrect password";
     UserDetailsService userDetailsService;
 
     @Autowired
@@ -32,6 +36,10 @@ public class InMemoryAuthenticationProvider implements AuthenticationProvider {
         }
         String password = credentials.toString();
         UserDetails userDetails = userDetailsService.loadUserByUsername(name);
+
+        if(!password.equals(userDetails.getPassword())) {
+            throw new BadCredentialsException(INCORRECT_PASSWORD);
+        }
 
         UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(name, password, userDetails.getAuthorities());
 
