0660ce4dc7d26cb84845e5f30cd54122a20bb131
Paweł Suchań <pawelsuchan94@gmail.com>
2023-08-11 13:35:49 +0200

    Continuation of the project

--- src/main/java/com/pawelsuc/SecurityConfig.java
+++ src/main/java/com/pawelsuc/SecurityConfig.java
@@ -1,6 +1,10 @@
 package com.pawelsuc;
 
+import com.pawelsuc.component.CustomDaoAuthenticationProvider;
+import com.pawelsuc.service.impl.JpaUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,6 +12,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity(debug = false)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    JpaUserDetailsService userDetailsService;
+    CustomDaoAuthenticationProvider authenticationProvider;
+
+    @Autowired
+    public SecurityConfig (JpaUserDetailsService userDetailsService, CustomDaoAuthenticationProvider authenticationProvider) {
+        this.userDetailsService = userDetailsService;
+        this.authenticationProvider = authenticationProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+        auth.authenticationProvider(authenticationProvider);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
--- src/main/java/com/pawelsuc/component/CustomDaoAuthenticationProvider.java
+++ src/main/java/com/pawelsuc/component/CustomDaoAuthenticationProvider.java
@@ -1,17 +1,55 @@
 package com.pawelsuc.component;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.util.Assert;
 
 public class CustomDaoAuthenticationProvider implements AuthenticationProvider {
+
+    private UserDetailsService userDetailsService;
+
+    private static final String USERNAME_CANNOT_BE_NULL = "Username cannot be null";
+    private static final String CREDENTIALS_CANNOT_BE_NULL = "Credentials cannot be null";
+    private static final String INCORRECT_PASSWORD = "Incorrect password";
+
+    
+    @Autowired
+    public CustomDaoAuthenticationProvider(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        return null;
+
+        String name = authentication.getName();
+        Object credentials = authentication.getCredentials();
+        Assert.notNull(name, USERNAME_CANNOT_BE_NULL);
+        Assert.notNull(credentials, CREDENTIALS_CANNOT_BE_NULL);
+        if(credentials instanceof String) {
+            return null;
+        }
+        String password = credentials.toString();
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(name);
+        boolean passwordMatch = userDetails.getPassword().equals(password);
+
+        if(!passwordMatch) {
+            throw new BadCredentialsException(INCORRECT_PASSWORD);
+        }
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(name, password, userDetails.getAuthorities());
+        return token;
     }
 
     @Override
-    public boolean supports(Class<?> aClass) {
-        return false;
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
     }
 }
